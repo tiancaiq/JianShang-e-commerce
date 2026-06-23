097c14402a47c5fe329c9a0e68eaaea28c7b29f8
tiancaiq <shen.ban@mycnmipss.org>
2026-06-22 22:48:36 -0700

    Sync deployable demo frontend and proxy config

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
